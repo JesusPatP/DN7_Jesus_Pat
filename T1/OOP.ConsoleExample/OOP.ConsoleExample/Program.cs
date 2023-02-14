19c8e06952afdb378b19379f0f6ee8a8a8e4dbc6
JHPAT100 <sanhool@live.com.mx>
2023-02-14 18:22:59 -0500

    First finished tutorial
    File folders from the first tutorial

---
+++ T1/OOP.ConsoleExample/OOP.ConsoleExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;
+
+namespace OOP.ConsoleExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console class with satatic method 
+            Console.WriteLine("Hello World!");
+
+            //Reference to class Animal in AudioManager
+            Animal animal = new Animal();
+            animal.AnimalSound();
+
+            //Wait a key to continue the program
+            Console.ReadKey();
+        }
+    }
+}
