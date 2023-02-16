5463d8813207f2894d9b5d67acf33be631ca1f38
JHPAT100 <sanhool@live.com.mx>
2023-02-15 19:18:56 -0500

    inherit from a class
    Inherit from a class

---
+++ 01_OOP/T2/OOP.ConsoleExample/AudioManager/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioManager
+{
+    public class Animal
+    {
+        public void AnimalSound()
+        {
+            Console.WriteLine("I'm an animal");
+        }
+    }
+}
---
+++ 01_OOP/T2/OOP.ConsoleExample/OOP.ConsoleExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;
+
+namespace OOP.ConsoleExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console class with satatic method 
+            Console.WriteLine("Hello World!");
+
+            //Reference to class Animal in AudioManager
+            Animal animal = new Animal();
+            //Invocation of the animal class
+            animal.AnimalSound();
+
+            //Access to the class
+            Elephant elephant = new Elephant();
+
+            //the class inherit from the animal class
+            elephant.AnimalSound();
+
+            //Wait a key to continue the program
+            Console.ReadKey();
+        }
+    }
+}
