caaf594f302019f5e8aee82eeba5d437e8b418cd
JHPAT100 <sanhool@live.com.mx>
2023-02-25 00:02:25 -0500

    4 DataTypes - Part 1

---
+++ 02_WebDevelopment/T4/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string text = "This is a string";
+
+int age = 35;
+
+DateTime now= DateTime.Now;
+
+double amount = 0;
+
+string string2 = text + " a second string " + age + now + amount;
+
+//Format the text
+string string3 = string.Format("The age is {0}, the time is {1}, and the amount is {2}",age,now,amount);
+
+//Format the text
+string string4 = $"The age is {age}, the time is {now}, and the amount is {amount}";
+
+char sampleChar = 'C';
+char[] arrayChar = string4.ToCharArray();
+
+
+for(int i = 0;i < string4.Length; i++)
+{
+    Console.WriteLine(string4[i]);
+}
+
+Console.WriteLine(sampleChar);
+
+Console.ReadKey();
