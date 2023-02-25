09d468f8729310e260190325ec8fd9fe6b40ee83
JHPAT100 <sanhool@live.com.mx>
2023-02-24 21:44:32 -0500

    3 MVCPattern

---
+++ 02_WebDevelopment/T3/GymManager.web/GymManager.web/Controllers/MembersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymManager.web.Controllers
+{
+    public class MembersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
