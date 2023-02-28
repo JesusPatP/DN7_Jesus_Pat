e6eafa88bdaa518f251f4bc1f9a2cce06f4d16ec
JHPAT100 <sanhool@live.com.mx>
2023-02-27 22:41:19 -0500

    5 Dom

---
+++ 02_WebDevelopment/E1/GymManager.web/GymManager.web/Models/Member.cs
@@ -0,0 +1,18 @@
+﻿namespace GymManager.web.Models
+{
+    public class Member
+    {
+
+        //Model Properties
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+
+        public int CityId { get; set; }
+
+        public string Email { get; set; }
+
+        public Boolean AllowNewsletter { get; set; }
+    }
+}
---
+++ 02_WebDevelopment/T5/DataTypes/DataTypes.Web/wwwroot/dataTypes.js
@@ -0,0 +1,53 @@
+﻿function onButtonClick(evt) {
+
+    var b = evt.currentTarget;
+
+    var classes = b.classList;
+
+    var isGreen = false;
+
+    for (var i = 0; i < classes.length; i++) {
+        var c = classes[i];
+
+        if (c == "greenButton") {
+            isGreen = true;
+            break;
+        }
+    }
+
+    if (isGreen) {
+        b.className = "btn redButton";
+    } else {
+        b.className = "btn greenButton";
+    }
+
+    var t = document.getElementById("sampleTable");
+    var currentHtml = t.innerHTML;
+
+    t.innerHTML += "<tr><td>" + b.getAttribute("name") + "</td><td>" + new Date() + "</td></tr>";
+
+
+
+    //var buttonName = b.getAttribute("name");
+    //debugger;
+    //alert("Click on: " + buttonName);
+
+};
+
+//to locate the button
+//var button = document.getElementsByTagName("button")[0];
+//var button = document.getElementById("button1");
+//var button = document.getElementsByName("testButton")[0];
+
+//var buttons = document.getElementsByClassName("btn");
+
+var buttons = document.getElementsByTagName("button");
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", onButtonClick);
+}
+
+//debugger;
+
+//button.addEventListener("click", onButtonClick);
+
