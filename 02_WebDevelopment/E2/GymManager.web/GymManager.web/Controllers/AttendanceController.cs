459b6cebc809daf12a9311060de7ebdc920b276e
JesusPatP <44218570+JesusPatP@users.noreply.github.com>
2023-03-07 13:12:33 -0500

    2.4.2 Creating Member IN / OUT views

---
+++ 02_WebDevelopment/E2/GymManager.web/GymManager.web/Controllers/AttendanceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymManager.web.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        public IActionResult MemberIn()
+        {
+            return View();
+        }
+
+        public IActionResult MemberOut()
+        {
+            return View();
+        }
+    }
+}
