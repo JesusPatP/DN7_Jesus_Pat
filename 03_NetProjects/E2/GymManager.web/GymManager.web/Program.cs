81c4b4f10196fd97b2b83b52c872d90ec210909a
JesusPatP <44218570+JesusPatP@users.noreply.github.com>
2023-03-14 23:36:33 -0500

    4 MembersCrudValidations

---
+++ 03_NetProjects/E2/GymManager.web/GymManager.web/Program.cs
@@ -0,0 +1,47 @@
+using GymManager.ApplicationServices.Members;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//method services
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+//builder.Services.AddSingleton<IMembersAppService, MembersAppService>();
+builder.Services.AddTransient<IMembersAppService, MembersAppService>();
+//builder.Services.AddScoped<IMembersAppService, MembersAppService>();
+
+var app = builder.Build();
+
+//Configure the localization middleware
+var defaultDateCulture = "es-Mx";
+var ci =  new CultureInfo(defaultDateCulture); 
+
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture(ci),
+    SupportedCultures = new List<CultureInfo>{
+    ci,
+    },
+    SupportedUICultures = new List<CultureInfo>{
+    ci,
+    },
+
+}); 
+
+
+//Tells the web service the source path.
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseHttpsRedirection();
+
+
+//Configuring a conventional route
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
